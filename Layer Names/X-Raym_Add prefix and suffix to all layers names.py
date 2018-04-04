6a0f1bfec9b641336ce034e6e3f0f2e9ee9152de
Raymond Radet <X-Raym@users.noreply.github.com>
2018-04-04 16:32:28 +0200

    Update X-Raym_Add prefix and suffix to all layers names.py

--- Layer Names/X-Raym_Add prefix and suffix to all layers names.py
+++ Layer Names/X-Raym_Add prefix and suffix to all layers names.py
@@ -1,3 +1,13 @@
+# Krita Script Name: Add prefix and suffix to all layers names
+# Instructions: Run.
+# Author: X-Raym
+# Author URI: https://www.extremraym.com
+# Repository: X-Raym/Krita-Scripts
+# Repository URI: https://github.com/X-Raym/Krita-Scripts/
+# Licence: GPL v3
+# Krita: 4.0.0
+# Version: 1.0
+
 import sys
 from krita import *
 
